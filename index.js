67a8acfab8e49e2470b57c7f0478e131317f65d4
Diogo Dourado <dcdourado@gmail.com>
2024-07-30 19:45:10 -0300

    Add testing endpoint with express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Oi")
+const express = require('express');
+const app = express();
+const port = process.argv[2] || 3000;
+
+app.get('/', (req, res) => {
+    console.log('Received a request in server with port: ', port);
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
